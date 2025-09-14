b57ffcc1927e973bdc259c189372e8b71d178658
Patricioerazo <patricio.erazo@hotmail.com>
2025-09-14 02:30:29 -0300

    perfiles_ok

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 // Variables
 const carrito = document.querySelector('#carrito');
-const listaCursos = document.querySelector('#lista-cursos');
+const listaCursos = document.querySelector('#productos');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito'); 
 
